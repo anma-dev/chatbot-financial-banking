570a0f0974bcb13cd23983d837fc883c69244cb5
anma-dev <andrijamatic0308@gmail.com>
2022-03-24 11:24:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+from urllib import response
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from chat import get_response
 
 app = Flask(__name__)
 CORS(app)
+
+@app.get("/")
+def index_get():
+   return render_template("base.html")
 
 @app.post("/predict")
 def predict():
